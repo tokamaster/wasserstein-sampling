7a0b023cd2af57d750c94e1fb9071546bff85342
tokamaster <enmidol@liverpool.ac.uk>
2021-12-20 01:15:44 +0000

    first

--- README.md
+++
@@ -1,1 +0,0 @@
-# wasserstein-sampling
---
+++ areametric/__init__.py
@@ -0,0 +1,2 @@
+from .areametric import (areaMe, Dataset, plot, areaMe_env, ParametricDataset)
+from .dists import (lognormal, Lognormal, normal, exponential, Exponential, uniform)
